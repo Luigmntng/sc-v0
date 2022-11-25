c700fb744f99986196c169dfdbe8e720811ff9c1
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-11-25 23:25:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ async function startHisoka() {
         mek = chatUpdate.messages[0]
         if (!mek.message) return
         mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-        if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+        if (mek.key && mek.key.remoteJid === 'status@broadcast') { setTimeout(() => { hisoka.readMessages([mek.key]) }, 2000); }
         if (!hisoka.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
         if (mek.key.id.startsWith('FatihArridho_')) return
